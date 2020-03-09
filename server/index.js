1baa37038fbddd05ca06ab191c1e7f09cba7bb46
Dustin Ancalade <umbi24@gmail.com>
2020-03-08 23:39:26 -0700

    cleaned up some commented out code

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8000;
-console.log('dirname', path.join(__dirname, 'public', 'index.html'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '..', 'public')));
