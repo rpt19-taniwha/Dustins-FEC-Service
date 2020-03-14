5e4a172f8608dc06f8b66c7dceb0cd1a129beb48
Dustin Ancalade <umbi24@gmail.com>
2020-03-14 11:25:35 -0700

    seeded db with urls of product list

--- database/dbHelper.js
+++ database/dbHelper.js
@@ -4,40 +4,43 @@ const createRandomQuantity = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-
-const maxImagesPerProfile = 10;
-
-const getProfileUrls = (url) => new Promise((resolve, reject) => {
-  return https.get((url, response) => {
-    const body = [];
-    response.on('data', (chunk) => body.concat(chunk));
-    response.on('end', () => resolve(JSON.stringify(body)));
-  }).on('error', reject);
-}).then((data) => {
-  console.log('data', data);
-}).catch((error) => {
-  console.log('error fetching data', error);
-});
-
 const createRandomImageProfiles = (images, maxImgQuantity) => {
     const singleProfileImages = [];
     const numberOfImages = createRandomQuantity(maxImgQuantity);
     for (let j = 0; j < numberOfImages; j++) {
       const index = createRandomQuantity(images.length);
-      console.log('index', index);
       singleProfileImages.push(images[index]);
     }
     console.log('singleProfileImages', singleProfileImages);
   return singleProfileImages;
+};
+
+const createSampleImageProfile = (images, numberOfImages) => {
+  const singleProfileImages = [];
+  for (let j = 0; j < numberOfImages; j++) {
+    singleProfileImages.push(images[j]);
+  }
+  console.log('singleProfileImages', singleProfileImages);
+return singleProfileImages;
+};
+
+
+const createImageUrl = (folderUrl, name, i, fileType) => {
+  return `${folderUrl}${name}/${i}/${fileType}`;
 
 };
 
 
 
+// https://s3-us-west-1.amazonaws.com/dustins.fec.product.images/Fec+pictures/image1.jpg
 
 
 
 
 
 
-module.exports = { profileImages: createRandomImageProfiles, getUrls: getProfileUrls};
+
+
+
+
+module.exports = { profileImages: createRandomImageProfiles, sampleImages: createSampleImageProfile, createImageUrl: createImageUrl };
