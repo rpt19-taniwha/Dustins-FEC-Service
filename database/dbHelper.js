6e10895432793b743ef36390b2bd97692b39d442
Dustin Ancalade <umbi24@gmail.com>
2020-03-14 13:20:36 -0700

    seeding of database is complete with refactoring of random image profiles to make sure the same image isn't grabbed twice

--- database/dbHelper.js
+++ database/dbHelper.js
@@ -1,44 +1,47 @@
 const https = require('https');
 
+// randomizer to be used to get a random index out of an array of picture urls
 const createRandomQuantity = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-const createRandomImageProfiles = (images, maxImgQuantity) => {
+// creates an array of a random number of random urls ready to be attached to a product object
+const createRandomImageProfiles = (images, maxImgQty) => {
+    //image urls
+    let imgs = [...images];
+    console.log('imgs', imgs);
+    // stores the image urls
     const singleProfileImages = [];
-    const numberOfImages = createRandomQuantity(maxImgQuantity);
+
+    // the number of urls to store in singleProfileImages
+    const numberOfImages = createRandomQuantity(maxImgQty) || 1;
+
+    //populates singleProfileImages with images urls
     for (let j = 0; j < numberOfImages; j++) {
-      const index = createRandomQuantity(images.length);
-      singleProfileImages.push(images[index]);
+      const index = createRandomQuantity(imgs.length);
+      singleProfileImages.push(imgs[index]);
+      //removes the used image from the imgs array to ensure no dupes
+      imgs.splice(index, 1);
     }
-    console.log('singleProfileImages', singleProfileImages);
   return singleProfileImages;
 };
 
+// creates an array of ordered urls for our sample product pokenatomy
 const createSampleImageProfile = (images, numberOfImages) => {
+  // stores images urls for sample
   const singleProfileImages = [];
+  // populates singleProfileImages with urls
   for (let j = 0; j < numberOfImages; j++) {
     singleProfileImages.push(images[j]);
   }
-  console.log('singleProfileImages', singleProfileImages);
-return singleProfileImages;
+  // console.log('singleProfileImages', singleProfileImages);
+return singleProfileImages;s
 };
 
-
+// constructs the full file path for each url from pieces to avoid hardcoding
 const createImageUrl = (folderUrl, name, i, fileType) => {
-  return `${folderUrl}${name}/${i}/${fileType}`;
-
+  return `${folderUrl}${name}/${i}${fileType}`;
 };
-
-
-
-// https://s3-us-west-1.amazonaws.com/dustins.fec.product.images/Fec+pictures/image1.jpg
-
-
-
-
-
-
 
 
 
