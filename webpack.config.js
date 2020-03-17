a6011d64cd1f7e825e812d2ba895070759672a8c
Dustin Ancalade <umbi24@gmail.com>
2020-03-16 22:32:48 -0700

    webpack configured and client renders

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const clientDir = path.join(__dirname, '/client');
+const publicDir = path.join(__dirname, '/public');
+console.log('publidDir', publicDir);
+console.log('clientDir', clientDir);
+
+module.exports = {
+  mode: "development",
+  entry: `${clientDir}/app.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: publicDir
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/i,
+        loader: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
