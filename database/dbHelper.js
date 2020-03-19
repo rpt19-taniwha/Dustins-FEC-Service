0e51458b6192020bdd14e208048fbe72183d6d2f
Dustin Ancalade <umbi24@gmail.com>
2020-03-18 21:50:20 -0700

    more cleanup in preparation for testing

--- database/dbHelper.js
+++ database/dbHelper.js
@@ -7,22 +7,22 @@ const createRandomQuantity = (max) => {
 
 // creates an array of a random number of random urls ready to be attached to a product object
 const createRandomImageProfiles = (images, maxImgQty) => {
-    //image urls
-    let imgs = [...images];
-    console.log('imgs', imgs);
-    // stores the image urls
-    const singleProfileImages = [];
+  //image urls
+  let imgs = [...images];
+  console.log('imgs', imgs);
+  // stores the image urls
+  const singleProfileImages = [];
 
-    // the number of urls to store in singleProfileImages
-    const numberOfImages = createRandomQuantity(maxImgQty) || 1;
+  // the number of urls to store in singleProfileImages
+  const numberOfImages = createRandomQuantity(maxImgQty) || 1;
 
-    //populates singleProfileImages with images urls
-    for (let j = 0; j < numberOfImages; j++) {
-      const index = createRandomQuantity(imgs.length);
-      singleProfileImages.push(imgs[index]);
-      //removes the used image from the imgs array to ensure no dupes
-      imgs.splice(index, 1);
-    }
+  //populates singleProfileImages with images urls
+  for (let j = 0; j < numberOfImages; j++) {
+    const index = createRandomQuantity(imgs.length);
+    singleProfileImages.push(imgs[index]);
+    //removes the used image from the imgs array to ensure no dupes
+    imgs.splice(index, 1);
+  }
   return singleProfileImages;
 };
 
@@ -35,7 +35,7 @@ const createSampleImageProfile = (images, numberOfImages) => {
     singleProfileImages.push(images[j]);
   }
   // console.log('singleProfileImages', singleProfileImages);
-return singleProfileImages;s
+  return singleProfileImages;
 };
 
 // constructs the full file path for each url from pieces to avoid hardcoding
