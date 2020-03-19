f6a78f07cbd2c16368957df35e9c8a1ffa7a963e
Dustin Ancalade <umbi24@gmail.com>
2020-03-18 22:20:56 -0700

    created first database tests and initialized a spec runner file

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,4 +1,26 @@
 const mocha = require('mocha');
 const chai = require('chai');
-const db = require('../database/index.js');
+const {db, productQuery} = require('../database/index.js');
+const server = require('../server/index.js');
 
+
+describe('database', function() {
+
+  const sampleProductId = '549504785';
+
+  it(`should return the sample Pokenatomy Object when product number ${sampleProductId} is queried`, function() {
+    productQuery({productId: sampleProductId}, (err, contents) => {
+      if (err) {
+        done(err);
+      } else {
+        expect(contents).to.have.property('imageUrls');
+        expect(contents.imageUrls.length).to.equal(8);
+        expect(contents.imageUrls[0]).to.equal('https://s3-us-west-1.amazonaws.com/dustins.fec.product.images/SampleProduct/pokenatomy/0.jpg');
+        expect(contents.imageUrls[8]).to.equal('https://s3-us-west-1.amazonaws.com/dustins.fec.product.images/SampleProduct/pokenatomy/7.jpg')
+        done();
+      }
+    });
+
+  });
+
+});
