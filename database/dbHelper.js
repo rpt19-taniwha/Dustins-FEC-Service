de010d078a072a4b02c3794b955c55cbd39c3112
Dustin Ancalade <umbi24@gmail.com>
2020-03-19 22:28:45 -0700

    fixed other styling

--- database/dbHelper.js
+++ database/dbHelper.js
@@ -1,4 +1,3 @@
-const https = require('https');
 
 // randomizer to be used to get a random index out of an array of picture urls
 const createRandomQuantity = (max) => {
@@ -7,22 +6,21 @@ const createRandomQuantity = (max) => {
 
 // creates an array of a random number of random urls ready to be attached to a product object
 const createRandomImageProfiles = (images, maxImgQty) => {
-    //image urls
-    let imgs = [...images];
-    console.log('imgs', imgs);
-    // stores the image urls
-    const singleProfileImages = [];
+  //image urls
+  let imgs = [...images];
+  // stores the image urls
+  const singleProfileImages = [];
 
-    // the number of urls to store in singleProfileImages
-    const numberOfImages = createRandomQuantity(maxImgQty) || 1;
+  // the number of urls to store in singleProfileImages
+  const numberOfImages = createRandomQuantity(maxImgQty) || 1;
 
-    //populates singleProfileImages with images urls
-    for (let j = 0; j < numberOfImages; j++) {
-      const index = createRandomQuantity(imgs.length);
-      singleProfileImages.push(imgs[index]);
-      //removes the used image from the imgs array to ensure no dupes
-      imgs.splice(index, 1);
-    }
+  //populates singleProfileImages with images urls
+  for (let j = 0; j < numberOfImages; j++) {
+    const index = createRandomQuantity(imgs.length);
+    singleProfileImages.push(imgs[index]);
+    //removes the used image from the imgs array to ensure no dupes
+    imgs.splice(index, 1);
+  }
   return singleProfileImages;
 };
 
@@ -35,15 +33,13 @@ const createSampleImageProfile = (images, numberOfImages) => {
     singleProfileImages.push(images[j]);
   }
   // console.log('singleProfileImages', singleProfileImages);
-return singleProfileImages;s
+  return singleProfileImages;
 };
 
 // constructs the full file path for each url from pieces to avoid hardcoding
 const createImageUrl = (folderUrl, name, i, fileType) => {
   return `${folderUrl}${name}/${i}${fileType}`;
 };
-
-
 
 
 module.exports = { profileImages: createRandomImageProfiles, sampleImages: createSampleImageProfile, createImageUrl: createImageUrl };
