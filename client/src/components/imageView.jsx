3ab6f62735648e7a9f1defeb412ea1cf4f38e369
Dustin Ancalade <umbi24@gmail.com>
2020-03-26 21:25:54 -0700

    db bug fixed and all images render

--- client/src/components/imageView.jsx
+++ client/src/components/imageView.jsx
@@ -7,6 +7,7 @@ import ImageList from './imageList.jsx';
 
 const ImageView = ({images, mainImage}) => (
   <div>
+    {console.log('images', images)}
     <ImageList images={images} />
     <MainImage mainImage={mainImage}/>
   </div>
