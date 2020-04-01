8cd8864fe94173aec748fb871b793509e9f20354
Dustin Ancalade <umbi24@gmail.com>
2020-03-31 19:28:03 -0700

    added cors fixed endpoing change name of jsx to image service

--- client/src/imageService.jsx
+++ client/src/imageService.jsx
@@ -12,7 +12,7 @@ library.add(fab, faChevronRight, faChevronLeft, faHeart);
 
 
 
-class App extends React.Component {
+class ImageService extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -67,4 +67,4 @@ class App extends React.Component {
 }
 
 
-ReactDom.render(<App />, document.getElementById('image'));
+ReactDom.render(<ImageService />, document.getElementById('image'));
