d761c00a909b1f6b15829802ce7256767586f232
Dustin Ancalade <umbi24@gmail.com>
2020-04-11 13:31:38 -0700

    cleaned up code and took out console.logs

--- server/index.js
+++ server/index.js
@@ -7,20 +7,12 @@ const port = process.env.Port || 8000;
 
 const rootPath = path.join(__dirname, '..', 'client/dist/')
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(rootPath));
 
-app.use(cors());
-
-app.use ((req, res, next) => {
-  console.log ('req', req.url);
-  next();
-
-})
 app.get('/listing/:productNumber', (req, res) => {
-    console.log('req.params', req.params);
-    console.log('rootpath', rootPath);
     res.sendFile('index.html', {
       root: `${rootPath}`
     });
