c785b13593cedf1d4b8c55c05f41fcff94034a93
Dustin Ancalade <umbi24@gmail.com>
2020-04-16 17:30:38 -0700

    seperated out production from development configs for webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, 'client/src');
+const DIST_DIR = path.join(__dirname, 'client/dist');
+
+module.exports = {
+  mode: 'production',
+  entry: `${SRC_DIR}/app.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+};
