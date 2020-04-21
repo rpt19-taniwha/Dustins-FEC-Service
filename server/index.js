79db3576328e64bc0f885005444ca44880b77f0f
Dustin Ancalade <umbi24@gmail.com>
2020-04-20 19:16:08 -0700

    fixed thumbnail bug on app.js which stopped the service from running

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
 const {db, productQuery} = require('../database/index.js');
-// const port = process.env.Port || 8000;
+const port = process.env.Port || 8000;
 
 // from localhost
-const port = process.env.Port || 8001;
+// const port = process.env.Port || 8001;
 
 const rootPath = path.join(__dirname, '..', 'client/dist/')
 
